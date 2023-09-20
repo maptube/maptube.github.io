92fcecdafc21cde960941f3bb14ac3bbf940d096
MapTube <richard.milton@ucl.ac.uk>
2023-09-20 16:10:20 +0100

    added droneshapes.js

---
+++ js/DroneShapes.js
@@ -0,0 +1,23 @@
+/**
+ * DroneShapes.js
+ * Contains drone positions for different 3D shapes in the sky.
+ * The lists are coordinates for the drones which are scaled later.
+ */
+
+export let DroneShapes = {
+    cube_verts_8: [
+        {x:-1,y:-1,z:-1,rgb:0xff0000},
+        {x:-1,y: 1,z:-1,rgb:0xff0000},
+        {x: 1,y: 1,z:-1,rgb:0xff0000},
+        {x: 1,y:-1,z:-1,rgb:0xff0000},
+        {x:-1,y:-1,z: 1,rgb:0xff0000},
+        {x:-1,y: 1,z: 1,rgb:0xff0000},
+        {x: 1,y: 1,z: 1,rgb:0xff0000},
+        {x: 1,y:-1,z: 1,rgb:0xff0000}
+    ],
+
+    cube2x2: [
+
+    ]
+
+}
